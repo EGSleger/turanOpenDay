bfc8c768c9dd8f09902d36988bc075204e70a13a
Yegor Kovnet <91253328+EGSleger@users.noreply.github.com>
2024-06-12 18:08:45 +0500

    Add files via upload

---
+++ mini app для турана/js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("open_model").addEventListener("click", function() {
+    document.getElementById("modal").classList.add("open")
+})
+
+document.getElementById("close_btn").addEventListener("click", function() {
+    document.getElementById("modal").classList.remove("open")
+})
+
+document.getElementById("reg_btn").addEventListener("click", function() {
+    document.getElementById("modal").classList.remove("open")
+})
